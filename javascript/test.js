59234f158fda936c7a678d3b8b342017e704f26e
Daniel Pantea <dani.pantea@gmail.com>
2022-10-13 08:57:42 +0300

    update 13oct

--- javascript/test.js
+++ javascript/test.js
@@ -1,8 +1,8 @@
 const btnPomodoro = document.getElementById('pomodoro')
 const btnShortbreak = document.getElementById('shortBreak')
 const btnLongbreak = document.getElementById('longBreak')
-const btnStartPause = document.getElementsByClassName('start-timer')
-const allBtns = document.getElementsByClassName('buttons')
+const btnStartPause = document.querySelector('.start-timer')
+const allBtns = document.querySelectorAll('button')
 const timeDisplayed = document.querySelector('.time')
 const circleElement = document.querySelector('circle')
 const audioStart = document.getElementsByClassName('start-audio')
@@ -19,19 +19,43 @@ const bntCloseSettings = document.getElementsByClassName('close-settings')
 const allInputs = document.querySelectorAll('input')
 
 
-let initialTimeSettings = {
+let setTime = {
     pomodoro: {
         minutes: 2,
+        seconds: 0
+    },
+    shortBreak: {
+        minutes: 3,
+        seconds: 0
+    },
+    longBreak: {
+        minutes: 4,
         seconds: 0
     }
 }
 
-btnPomodoro.addEventListener('click', () => {
-    timeDisplayed.innerText = `${initialTimeSettings.pomodoro.minutes}:0${initialTimeSettings.pomodoro.seconds}`;
-})
-
 allBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        timeDisplayed.innerText = `${initialTimeSettings.pomodoro.minutes}:0${initialTimeSettings.pomodoro.seconds}`;
+        timeDisplayed.innerText = `${setTime[button.id].minutes}:0${setTime[button.id].seconds}`;
+        highlightActiveBtn(button);
     })
 })
+
+function highlightActiveBtn(button) {
+    allBtns.forEach((element) => {
+        element.classList.remove('active');
+    })
+    button.classList.add('active');
+}
+
+btnStartPause.addEventListener('click', () => {
+    changeStartPauseBtnState();
+})
+
+function changeStartPauseBtnState() {
+    if(btnStartPause.innerText === 'start'.toUpperCase()) {
+        btnStartPause.innerText = 'pause'.toUpperCase();
+    } else {
+        btnStartPause.innerText = 'start'.toUpperCase();
+    }
+}
